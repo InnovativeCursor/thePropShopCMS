968cb7433cc65f57177450663fee535da57e8b2a
Shawn Almeida <shawn9920@gmail.com>
2024-05-21 02:37:57 +0530

    starting of with filter menu

--- client/src/Components/createProd/AddProduct.jsx
+++ client/src/Components/createProd/AddProduct.jsx
@@ -1,12 +1,4 @@
 import React, { useEffect, useState } from "react";
-import PageWrapper from "../PageContainer/PageWrapper";
-import { Form, Input, Spin, Checkbox, Upload, InputNumber } from "antd";
-import TextArea from "antd/es/input/TextArea";
-import { PlusOutlined } from "@ant-design/icons";
-import Creatable from "react-select/creatable";
-import Swal from "sweetalert2";
-import { getAxiosCall, postAxiosCall } from "../../Axios/UniversalAxiosCalls";
-import { useNavigate } from "react-router-dom";
 import GlobalForm from "../GlobalForm/GlobalForm";
 
 export default function AddProduct(props) {
