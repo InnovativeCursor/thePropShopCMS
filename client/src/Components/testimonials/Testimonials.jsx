df580ea98438c1a7628747bb28d3e4700439a828
Shawn Almeida <shawn9920@gmail.com>
2024-05-30 23:38:47 +0530

    Created Testimonial FE and BE

---
+++ client/src/Components/testimonials/Testimonials.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PageWrapper from "../../Components/PageContainer/PageWrapper";
+import GlobalForm from "../GlobalForm/GlobalForm";
+function Testimonials() {
+  return <GlobalForm type="Testimonials" pageMode="Add" />;
+}
+
+export default Testimonials;
